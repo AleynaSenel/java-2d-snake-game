e7ec5b5e779587a6d739e1476a16c26914ff8243
AleynaSenel <aleysenell@gmail.com>
2026-03-15 03:56:11 +0300

    Yilanin buyumesi ve kuyruk hareketi eklendi

--- GamePanel.java
+++ GamePanel.java
@@ -15,8 +15,14 @@ public class GamePanel extends JPanel implements ActionListener {
     private final int Screen_Height = 600;
     private final int Unit_Size = 25;      //yılan her adımda 25 piksel hareket edecek
 
-    private  int location_x = 200;
-    private  int location_y = 200;
+    int ColumnsSize = Screen_Width / Unit_Size;
+    int RowSize = Screen_Height / Unit_Size;
+    int maxUnitSize = ColumnsSize * RowSize;
+
+    int[] location_x = new int[maxUnitSize];
+    int[] location_y = new int[maxUnitSize];
+
+    int bodyParts = 1;
 
     private final int delay = 75;    // her 75 milisaniye de actionPerformed metodu tetikleniyor
     Timer timer;
@@ -52,16 +58,20 @@ public class GamePanel extends JPanel implements ActionListener {
 
         System.out.println("Zaman isliyor, oyun dongusu calisiyor!");
 
+        move();
+
         //location_x = location_x + Unit_Size;
         switch(direction){
-            case 'R':  location_x = location_x + Unit_Size;   break;
-            case 'L':  location_x = location_x - Unit_Size;   break;
-            case 'U':   location_y = location_y - Unit_Size;  break;
-            case 'D':   location_y = location_y + Unit_Size;  break;
+            case 'R':  location_x[0] = location_x[0] + Unit_Size;   break;
+            case 'L':  location_x[0] = location_x[0] - Unit_Size;   break;
+            case 'U':  location_y[0] = location_y[0] - Unit_Size;   break;
+            case 'D':  location_y[0] = location_y[0] + Unit_Size;   break;
 
         }
 
+        
         checkFood();
+        
 
         repaint();     //koordinatlar değişti, paintComponent'i tekrar çalıştır der
     } 
@@ -69,6 +79,7 @@ public class GamePanel extends JPanel implements ActionListener {
     public void draw(Graphics g){
 
         g.setColor(Color.GRAY); // Izgara çizgileri gri olsun
+        
         for(int i = 0; i < Screen_Width / Unit_Size; i++) {
         g.drawLine(i * Unit_Size, 0, i * Unit_Size, Screen_Height); // Dikey çizgiler
         g.drawLine(0, i * Unit_Size, Screen_Width, i * Unit_Size); // Yatay çizgiler
@@ -79,8 +90,14 @@ public class GamePanel extends JPanel implements ActionListener {
         g.setColor(Color.RED);
         g.fillOval(food.x, food.y, Unit_Size, Unit_Size);
 
-        g.setColor(Color.black);
-        g.fillRect(location_x, location_y, Unit_Size, Unit_Size);
+       
+
+        for(int i=0; i< bodyParts; i++){
+            
+            g.setColor(Color.black); 
+
+            g.fillRect(location_x[i], location_y[i], Unit_Size, Unit_Size);
+        }
 
         
 
@@ -125,11 +142,21 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void checkFood(){
-        if((food.x == location_x) &&(food.y == location_y)){
+        if((food.x == location_x[0]) && (food.y == location_y[0])){
             food.generateNewPosition(Screen_Width, Screen_Height, Unit_Size);
+            bodyParts++;
         }
 
     }
+
+    public void move(){
+        for(int k = bodyParts; k > 0 ;k--){
+            location_x[k] = location_x[k-1];
+            location_y[k] = location_y[k-1];
+        }
+    }
+
+
 
     
     
